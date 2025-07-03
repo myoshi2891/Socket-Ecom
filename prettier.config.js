e7ddfaf071284dddd50e59d3cd41ce8a2d81c6ef
myoshizumi <myoshizumi@alumni.berklee.edu>
2025-07-03 19:39:22 +0900

    251. Handling Socket Connections and Managing Watcher Logic

---
+++ prettier.config.js
@@ -0,0 +1,9 @@
+// prettier.config.js
+/** @type {import('prettier').Config} */
+module.exports = {
+    semi: true,
+    singleQuote: false,
+    tabWidth: 4,
+    trailingComma: 'es5',
+    plugins: ['prettier-plugin-tailwindcss'],
+}
--- server.js
+++ server.js
@@ -13,7 +13,52 @@ const server = express()
 const httpServer = createServer(server)
 
 // 2. Setup WebSocket Server
+const wss = new WebSocket.Server({ server: httpServer })
+const watchers = new Map()
 
+wss.on('connection', (ws, res) => { 
+    const productId = req.url && req.url.split('/').pop()
+    if (!productId) return 
+
+    // Increment watcher count
+    const currentCount = (watchers.get(productId) || 0) + 1
+    watchers.set(productId, currentCount)
+    console.log(`New connection for product ${productId} with ${currentCount} watchers`);
+    
+    // Notify all connected clients about the new watcher count
+    wss.clients.forEach((client) => {
+        if (client.readyState === WebSocket.OPEN) {
+            client.send(JSON.stringify({ productId, count: currentCount }))
+        }
+    })
+
+    // Handle disconnection
+    ws.on("close", () => {
+        const updatedCount = Math.max((watchers.get(productId) || 0) - 1, 0)
+        if (updatedCount === 0) {
+            watchers.delete(productId) // Remove entry if no watchers left
+        } else { 
+        watchers.set(productId, updatedCount)
+        }
+        console.log(`Connection closed for product ${productId} : ${updatedCount} watchers`);
+
+        // Notify all clients about the updated watcher count
+        wss.clients.forEach((client) => {
+            if (client.readyState === WebSocket.OPEN) {
+                client.send(JSON.stringify({ productId, count: currentCount }))
+            }
+        })
+    })
+
+    // Send initial message
+    ws.send(
+        JSON.stringify({
+            message: 'Connected to the WebSocket server!',
+            productId,
+            count: currentCount
+        })
+    )
+})
 
 // 3. Handle Next.js Routing
 server.all('*', (req, res) => {
